5a6c38bbbbed0f22209e7ace9fcf88c45f6070d6
Gustavo Willemann <guswillemann@gmail.com>
2022-12-21 16:20:56 -0300

    add precense colors to avatars

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,9 +1,19 @@
+import getPresenceColor from '../../utils/getPresenceColor';
 import style from './styles.module.css';
 
 export default function Avatar({ other }) {
   return (
-    <li className={style.wrapper}>
-      <img width={50} height={50} src={other.presence.avatar} />
+    <li
+      className={style.wrapper}
+      style={{
+        '--presence-color': getPresenceColor(other.connectionId)
+      }}
+    >
+      <img
+        width={50}
+        height={50}
+        src={other.presence.avatar}
+      />
       <span className={style.tooltip}>{other.presence.username}</span>
     </li>
   )
